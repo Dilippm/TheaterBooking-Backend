9b508ce357a16d92f5077eeb64fa32a782d866bb
Dilippm <dilippm92@gmail.com>
2024-09-06 14:22:02 +0530

    code updated

--- config/stripe.go
+++ config/stripe.go
@@ -2,9 +2,10 @@
 package config
 
 import (
+	
 	"github.com/stripe/stripe-go"
 )
 
 func InitStripe() {
-	stripe.Key = "sk_test_51NJuUZSAAYZdIKiYnHnXVSR90b8YaJeFE80htk0yFwkJwxlDPiypBUsIh3TYhvQv5PgA7oGx4H1jRB0OESwBvzMf0031WQaE3t" 
+	stripe.Key = STRIPE_KEY
 }
